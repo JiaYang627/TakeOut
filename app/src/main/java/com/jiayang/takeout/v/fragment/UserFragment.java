38c724c2acfdf44f1078de30e47fabb43f92d215
JiaYang <JiaYang627@163.com>
2017-09-29 11:43:06 +0800

    个人显示优化

--- app/src/main/java/com/jiayang/takeout/v/fragment/UserFragment.java
+++ app/src/main/java/com/jiayang/takeout/v/fragment/UserFragment.java
@@ -6,11 +6,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.alibaba.fastjson.JSON;
 import com.jiayang.takeout.R;
+import com.jiayang.takeout.common.Constants;
 import com.jiayang.takeout.m.component.ApiComponent;
+import com.jiayang.takeout.ormdao.bean.UserBean;
 import com.jiayang.takeout.p.fragment.UserFragmentPst;
-import com.jiayang.takeout.utils.LogUtils;
+import com.jiayang.takeout.utils.PreferenceTool;
 import com.jiayang.takeout.v.base.BaseFragment;
 import com.jiayang.takeout.v.iview.IuserFragmentView;
 
@@ -29,6 +34,12 @@ public class UserFragment extends BaseFragment<UserFragmentPst> implements Iuser
     ImageView mTvUserSetting;
     @BindView(R.id.login)
     ImageView mLogin;
+    @BindView(R.id.username)
+    TextView mUsername;
+    @BindView(R.id.phone)
+    TextView mPhone;
+    @BindView(R.id.ll_userinfo)
+    LinearLayout mLlUserinfo;
     private Unbinder mUnbinder;
 
     @Override
@@ -48,6 +59,22 @@ public class UserFragment extends BaseFragment<UserFragmentPst> implements Iuser
     @Override
     public void onResume() {
         super.onResume();
+
+        String userInfo = PreferenceTool.getString(Constants.SP_Info.SP_USER_INFO, "");
+        UserBean userBean = JSON.parseObject(userInfo, UserBean.class);
+        if (userBean != null) {
+
+            //立即登录 隐藏
+            mLogin.setVisibility(View.GONE);
+
+            mUsername.setText(userBean.name);
+            mPhone.setText(userBean.phone.substring(0, 3) + "****" + userBean.phone.substring(7, 11));
+            mLlUserinfo.setVisibility(View.VISIBLE);
+
+        } else {
+            mLogin.setVisibility(View.VISIBLE);
+            mLlUserinfo.setVisibility(View.GONE);
+        }
     }
 
     @Override
